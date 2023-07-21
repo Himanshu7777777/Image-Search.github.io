a0bfa1fe7f2468c6a44c0288dd53c00110b3cb3f
Himanshu7777777 <134919070+Himanshu7777777@users.noreply.github.com>
2023-07-21 17:02:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// const accessKey="r4pii6fVbz28qOng-Mipxa7qxL4WFGxWb3mrM4nvmCo"
+
+// const formEl =document.querySelector("form")
+// const inputEl= document.getElementById("search-input")
+// const  searchResults = document.querySelector(".search-results")
+// const showMore = document.getElementById("show")
+
+// let inputData=""
+// let page=1;
+
+// function searchImages(){
+//     inputData = inputEl.value;
+//     const url = `https://api`
+// }
+function d(){
+    let a=document.getElementById("search-button")
+    let b=document.getElementById("search-input")
+    
+    // a.style.color="red"
+    // a.innerHTML()
+}
+function c(nu){
+    nu =parseInt(nu)
+   let b=document.querySelectorAll(".search-result")
+   b[nu].style.height="31%"
+   b[nu].style.width="31%"
+}
+function d(nu){
+    nu=parseInt(nu)
+    let b=document.querySelectorAll(".search-result")
+   b[nu].style.height="100%"
+   b[nu].style.width="30%"
+
+}
+
+
